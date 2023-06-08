dd0f0504b25a27faa0bc64b453145d964022ed4b
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-08 16:49:16 +0700

    rename function

--- app/src/main/java/id/furqoncreative/jetstories/ui/theme/Theme.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/theme/Theme.kt
@@ -73,7 +73,7 @@ private val DarkColors = darkColorScheme(
 )
 
 @Composable
-fun AppTheme(
+fun JetStoriesTheme(
   useDarkTheme: Boolean = isSystemInDarkTheme(),
   content: @Composable() () -> Unit
 ) {
