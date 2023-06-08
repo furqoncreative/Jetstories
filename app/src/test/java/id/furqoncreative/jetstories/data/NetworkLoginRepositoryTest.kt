854fd00332d54c59b64a2bf44bfb763326c17126
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-08 15:53:41 +0700

    passed test

--- app/src/test/java/id/furqoncreative/jetstories/data/NetworkLoginRepositoryTest.kt
+++ app/src/test/java/id/furqoncreative/jetstories/data/NetworkLoginRepositoryTest.kt
@@ -1,5 +1,11 @@
 package id.furqoncreative.jetstories.data
 
+import id.furqoncreative.jetstories.data.repository.NetworkLoginRepository
+import id.furqoncreative.jetstories.fake.FakeApiService
+import id.furqoncreative.jetstories.fake.FakeDataSource
+import id.furqoncreative.jetstories.model.login.LoginResponse
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class NetworkLoginRepositoryTest {
@@ -7,7 +13,22 @@ class NetworkLoginRepositoryTest {
     @Test
     fun givenInvalidUsernameAndInvalidPasswordThenShouldFail() =
         runTest {
+            val actualResult = LoginResponse(
+                error = true,
+                message = "\\\"email\\\" must be a valid email",
+                loginResult = null
+            )
 
+            val expectedResult = FakeDataSource.errorLoginResponse
+
+            val repository = NetworkLoginRepository(
+                apiService = FakeApiService(actualResult)
+            )
+
+            assertEquals(
+                expectedResult,
+                repository.loginUser( email = "email.com", password = "1234")
+            )
         }
-
 }
+
--- app/src/test/java/id/furqoncreative/jetstories/fake/FakeApiService.kt
+++ app/src/test/java/id/furqoncreative/jetstories/fake/FakeApiService.kt
@@ -1,7 +1,10 @@
 package id.furqoncreative.jetstories.fake
 
 import id.furqoncreative.jetstories.data.source.network.response.JetstoriesApiService
+import id.furqoncreative.jetstories.model.login.LoginResponse
 
-class FakeApiService: JetstoriesApiService {
-
+class FakeApiService(
+    val response: Any
+): JetstoriesApiService {
+    override suspend fun loginUser(email: String, password: String): LoginResponse = response as LoginResponse
 }
