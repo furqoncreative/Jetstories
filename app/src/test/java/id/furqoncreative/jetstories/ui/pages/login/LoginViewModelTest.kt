d84bba5371c63f0feed9f51fcf99fb84668beef4
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-08 16:51:52 +0700

    implement `TestDispatcherRule`

--- app/src/test/java/id/furqoncreative/jetstories/ui/pages/login/LoginViewModelTest.kt
+++ app/src/test/java/id/furqoncreative/jetstories/ui/pages/login/LoginViewModelTest.kt
@@ -1,12 +1,17 @@
 package id.furqoncreative.jetstories.ui.pages.login
 
+import com.example.marsphotos.rules.TestDispatcherRule
 import id.furqoncreative.jetstories.fake.FakeDataSource
 import id.furqoncreative.jetstories.fake.FakeNetworkLoginRepository
 import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.test.runTest
+import org.junit.Rule
 import org.junit.Test
 
 class LoginViewModelTest {
+
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
 
     @Test
     fun whenUserSuccessLoginThenVerifyLoginUiStateSuccess() = runTest {
