4b7c45de4f94cf7552cb45b8db0e7a904ea0fcad
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-08 15:12:48 +0700

    add and implement 'loginUser' method

--- app/src/main/java/id/furqoncreative/jetstories/data/repository/LoginRepository.kt
+++ app/src/main/java/id/furqoncreative/jetstories/data/repository/LoginRepository.kt
@@ -1,13 +1,14 @@
 package id.furqoncreative.jetstories.data.repository
 
 import id.furqoncreative.jetstories.data.source.network.response.JetstoriesApiService
+import id.furqoncreative.jetstories.model.login.LoginResponse
 
 interface LoginRepository {
-
+    suspend fun loginUser(email: String, password: String): LoginResponse
 }
 
 class NetworkLoginRepository(
-    apiService: JetstoriesApiService
+    private val apiService: JetstoriesApiService
 ): LoginRepository {
-
+    override suspend fun loginUser(email: String, password: String) = apiService.loginUser(email, password)
 }
