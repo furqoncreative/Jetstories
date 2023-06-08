9fd1b09ea8b2acae1ec9720bab0ddd91b50e879a
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-08 17:03:12 +0700

    add `loginUser` method in view model

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/LoginViewModel.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/LoginViewModel.kt
@@ -3,8 +3,12 @@ package id.furqoncreative.jetstories.ui.pages.login
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import id.furqoncreative.jetstories.data.repository.LoginRepository
 import id.furqoncreative.jetstories.model.login.LoginResponse
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 sealed interface LoginUiState {
     data class Success(val loginResponse: LoginResponse) : LoginUiState
@@ -13,8 +17,22 @@ sealed interface LoginUiState {
 }
 
 class LoginViewModel(
-    loginRepository: LoginRepository
-) {
+    val loginRepository: LoginRepository
+) : ViewModel() {
+
     var loginUiState: LoginUiState by mutableStateOf(LoginUiState.Loading)
         private set
+
+    fun loginUser(email: String, password: String) {
+        viewModelScope.launch {
+            loginUiState = LoginUiState.Loading
+            loginUiState = try {
+                LoginUiState.Success(loginRepository.loginUser(email, password))
+            } catch (e: Exception) {
+                LoginUiState.Error
+            } catch (e: HttpException) {
+                LoginUiState.Error
+            }
+        }
+    }
 }
