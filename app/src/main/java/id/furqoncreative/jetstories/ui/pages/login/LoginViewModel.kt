2ef0ccab080c754cfa349acc71f01c093d06e9c1
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-08 16:43:57 +0700

    create `LoginViewModel` class

---
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package id.furqoncreative.jetstories.ui.pages.login
+
+import id.furqoncreative.jetstories.data.repository.LoginRepository
+
+class LoginViewModel(
+    loginRepository: LoginRepository
+) {
+}
