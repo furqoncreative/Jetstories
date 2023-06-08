c16488ac5e96a13474048cabb1cc91847b25f06f
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-08 14:48:46 +0700

    create `FakeApiService` class

---
+++ app/src/test/java/id/furqoncreative/jetstories/fake/FakeApiService.kt
@@ -0,0 +1,7 @@
+package id.furqoncreative.jetstories.fake
+
+import id.furqoncreative.jetstories.data.source.network.response.JetstoriesApiService
+
+class FakeApiService: JetstoriesApiService {
+
+}
