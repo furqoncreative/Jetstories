1a17e99cea879b474894790a72b6fad12d6d1b85
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-08 16:48:59 +0700

    add `LoginUiState` sealed interface

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/LoginViewModel.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/LoginViewModel.kt
@@ -1,8 +1,20 @@
 package id.furqoncreative.jetstories.ui.pages.login
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import id.furqoncreative.jetstories.data.repository.LoginRepository
+import id.furqoncreative.jetstories.model.login.LoginResponse
+
+sealed interface LoginUiState {
+    data class Success(val loginResponse: LoginResponse) : LoginUiState
+    object Error : LoginUiState
+    object Loading : LoginUiState
+}
 
 class LoginViewModel(
     loginRepository: LoginRepository
 ) {
+    var loginUiState: LoginUiState by mutableStateOf(LoginUiState.Loading)
+        private set
 }
