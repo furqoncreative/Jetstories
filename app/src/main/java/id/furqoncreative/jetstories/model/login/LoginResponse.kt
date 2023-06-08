7b8c25467c11edea9049d2bd9d794c1cd36905d8
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-08 14:59:59 +0700

    create `LoginResponse` data class

---
+++ app/src/main/java/id/furqoncreative/jetstories/model/login/LoginResponse.kt
@@ -0,0 +1,15 @@
+package id.furqoncreative.jetstories.model.login
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginResponse(
+    @SerialName("error")
+    val error: Boolean?,
+    @SerialName("loginResult")
+    val loginResult: LoginResult?,
+    @SerialName("message")
+    val message: String?
+)
---
+++ app/src/main/java/id/furqoncreative/jetstories/model/login/LoginResult.kt
@@ -0,0 +1,15 @@
+package id.furqoncreative.jetstories.model.login
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginResult(
+    @SerialName("name")
+    val name: String?,
+    @SerialName("token")
+    val token: String?,
+    @SerialName("userId")
+    val userId: String?
+)
