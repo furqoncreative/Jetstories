824ffd1a15cd128dce8655a5de15993a0455ac76
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-08 15:05:40 +0700

    add `errorLoginResponse` to FakeDataSource

--- app/src/test/java/id/furqoncreative/jetstories/fake/FakeDataSource.kt
+++ app/src/test/java/id/furqoncreative/jetstories/fake/FakeDataSource.kt
@@ -1,7 +1,11 @@
 package id.furqoncreative.jetstories.fake
 
+import id.furqoncreative.jetstories.model.login.LoginResponse
+
 object FakeDataSource {
     val errorLoginResponse = LoginResponse(
-
+        error = true,
+        message = "\\\"email\\\" must be a valid email",
+        loginResult = null
     )
 }
