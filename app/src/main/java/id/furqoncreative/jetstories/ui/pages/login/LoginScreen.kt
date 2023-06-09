346a42f1af62d49ed4fb8d5cd7e394af27f44e0f
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-09 17:47:52 +0700

    add login body layout

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/LoginScreen.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/LoginScreen.kt
@@ -1,14 +1,20 @@
 package id.furqoncreative.jetstories.ui.pages.login
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import id.furqoncreative.jetstories.ui.pages.login.components.LoginBody
 import id.furqoncreative.jetstories.ui.pages.login.components.LoginHeader
 import id.furqoncreative.jetstories.ui.theme.JetStoriesTheme
 
@@ -16,7 +22,18 @@ import id.furqoncreative.jetstories.ui.theme.JetStoriesTheme
 fun LoginScreen(
     modifier: Modifier = Modifier
 ) {
-    LoginHeader(modifier = modifier)
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .verticalScroll(state = rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        LoginHeader(modifier = modifier)
+
+        LoginBody(modifier = modifier)
+    }
 }
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
@@ -24,10 +41,7 @@ fun LoginScreen(
 fun LoginScreenPreview() {
     JetStoriesTheme {
         Surface(
-            color = MaterialTheme.colorScheme.background,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp)
+            color = MaterialTheme.colorScheme.background, modifier = Modifier.fillMaxSize()
         ) {
             LoginScreen()
         }
---
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/components/LoginBody.kt
@@ -0,0 +1,77 @@
+package id.furqoncreative.jetstories.ui.pages.login.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.furqoncreative.jetstories.ui.theme.JetStoriesTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginBody(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier, verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        OutlinedTextField(value = "", label = {
+            Text(text = "Email")
+        }, placeholder = {
+            Text(text = "Masukan email Anda")
+        }, onValueChange = {
+
+        }, modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(value = "", label = {
+            Text(text = "Password")
+        }, placeholder = {
+            Text(text = "Masukan password Anda")
+        }, onValueChange = {
+
+        }, modifier = Modifier.fillMaxWidth()
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.End),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            Text(
+                text = "Don't have an account?"
+            )
+            Text(
+                text = "Sign up",
+                color = MaterialTheme.colorScheme.tertiary,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+        }
+
+        Button(modifier = Modifier
+            .height(56.dp)
+            .fillMaxWidth(), onClick = { /*TODO*/ }) {
+            Text(text = "Sign in")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginBodyPreview() {
+    JetStoriesTheme {
+        LoginBody()
+    }
+}
