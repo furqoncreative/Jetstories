b65d00b7c24e5ff34627871a403dee13efb7ea9b
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-09 17:26:35 +0700

    update login header

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/components/LoginHeader.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/components/LoginHeader.kt
@@ -2,7 +2,10 @@ package id.furqoncreative.jetstories.ui.pages.login.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import id.furqoncreative.jetstories.R
 
@@ -18,23 +22,34 @@ import id.furqoncreative.jetstories.R
 fun LoginHeader(
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(10.dp)
+    Box(
+        contentAlignment = Alignment.Center, modifier = modifier
+            .height(300.dp)
+            .fillMaxWidth()
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.jetstories_logo),
-            contentDescription = "Jetstories Logo",
-            modifier = Modifier.size(125.dp)
-        )
-        Text(
-            text = stringResource(id = R.string.app_name),
-            style = MaterialTheme.typography.headlineLarge,
-        )
-        Text(
-            text = stringResource(id = R.string.app_tagline),
-            style = MaterialTheme.typography.titleSmall,
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.jetstories_logo),
+                contentDescription = "Jetstories Logo",
+                modifier = Modifier.size(125.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.headlineLarge,
+            )
+            Text(
+                text = stringResource(id = R.string.app_tagline),
+                style = MaterialTheme.typography.titleSmall,
+            )
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginHeaderPreview() {
+    LoginHeader()
 }
