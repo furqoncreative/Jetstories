541bcb2c7d1462b1ef91bd4d7f1c298342e35053
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-12 16:35:49 +0700

    create `JetstoriesApplication` class

---
+++ app/src/main/java/id/furqoncreative/jetstories/JetstoriesApplication.kt
@@ -0,0 +1,14 @@
+package id.furqoncreative.jetstories
+
+import android.app.Application
+import id.furqoncreative.jetstories.data.AppContainer
+import id.furqoncreative.jetstories.data.DefaultAppContainer
+
+class JetstoriesApplication : Application() {
+
+    private lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = DefaultAppContainer()
+    }
+}
