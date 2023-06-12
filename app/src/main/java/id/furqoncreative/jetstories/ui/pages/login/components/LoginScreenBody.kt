f7a89832134d60bdb04f6798dd4cba31ddaa4bf3
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-12 16:43:48 +0700

    extract state

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/components/LoginScreenBody.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/components/LoginScreenBody.kt
@@ -10,38 +10,32 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import id.furqoncreative.jetstories.ui.components.EmailState
-import id.furqoncreative.jetstories.ui.components.EmailStateSaver
 import id.furqoncreative.jetstories.ui.components.EmailTextField
 import id.furqoncreative.jetstories.ui.components.PasswordState
 import id.furqoncreative.jetstories.ui.components.PasswordTextField
 import id.furqoncreative.jetstories.ui.theme.JetStoriesTheme
+import id.furqoncreative.jetstories.util.TextFieldState
 
 @Composable
 fun LoginBody(
+    emailState: TextFieldState,
+    passwordState: PasswordState,
+    onSubmit: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier, verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
 
-        val emailState by rememberSaveable(stateSaver = EmailStateSaver) {
-            mutableStateOf(EmailState())
-        }
-
-        val passwordState = remember { PasswordState() }
-
         EmailTextField(emailState = emailState)
 
-        PasswordTextField(passwordState = passwordState)
+        PasswordTextField(passwordState = passwordState, onImeAction = {
+            onSubmit()
+        })
 
         Row(
             modifier = Modifier
@@ -61,7 +55,9 @@ fun LoginBody(
 
         Button(modifier = Modifier
             .height(56.dp)
-            .fillMaxWidth(), onClick = { /*TODO*/ }) {
+            .fillMaxWidth(), onClick = {
+            onSubmit()
+        }) {
             Text(text = "Sign in")
         }
     }
@@ -71,6 +67,6 @@ fun LoginBody(
 @Composable
 fun LoginBodyPreview() {
     JetStoriesTheme {
-        LoginBody()
+        LoginBody(emailState = TextFieldState(), passwordState = PasswordState(), onSubmit = {})
     }
 }
