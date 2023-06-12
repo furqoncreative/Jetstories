04e81a5d9c9acfb14e42e614afffeed78bf83b57
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-12 14:45:50 +0700

    create `EmailTextField` component

---
+++ app/src/main/java/id/furqoncreative/jetstories/ui/components/EmailTextField.kt
@@ -0,0 +1,27 @@
+package id.furqoncreative.jetstories.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EmailTextField(
+    emailState: TextFieldState = remember { EmailState() },
+    onImeAction: () -> Unit = {}
+) {
+
+    OutlinedTextField(value = "", label = {
+        Text(text = "Email")
+    }, placeholder = {
+        Text(text = "Masukan email Anda")
+    }, onValueChange = {
+
+    }, modifier = Modifier.fillMaxWidth()
+    )
+
+}
