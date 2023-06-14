b10497d14c9b0e4886cb1d413030d6fc8adfc70e
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-14 16:39:14 +0700

    add `provideNetworkDataSource` dependency injection

--- app/src/main/java/id/furqoncreative/jetstories/data/repository/LoginRepository.kt
+++ app/src/main/java/id/furqoncreative/jetstories/data/repository/LoginRepository.kt
@@ -1,15 +1,17 @@
 package id.furqoncreative.jetstories.data.repository
 
+import id.furqoncreative.jetstories.data.source.network.JestoriesNetworkDataSource
 import id.furqoncreative.jetstories.data.source.network.JetstoriesApiService
 import id.furqoncreative.jetstories.model.login.LoginResponse
+import javax.inject.Inject
 
 interface LoginRepository {
     suspend fun loginUser(email: String, password: String): LoginResponse
 }
 
-class NetworkLoginRepository(
-    private val apiService: JetstoriesApiService
+class NetworkLoginRepository @Inject constructor(
+    private val networkDataSource: JestoriesNetworkDataSource
 ) : LoginRepository {
     override suspend fun loginUser(email: String, password: String) =
-        apiService.loginUser(email, password)
+        networkDataSource.loginUser(email, password)
 }
--- app/src/main/java/id/furqoncreative/jetstories/di/DataModules.kt
+++ app/src/main/java/id/furqoncreative/jetstories/di/DataModules.kt
@@ -6,10 +6,12 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import id.furqoncreative.jetstories.BuildConfig.BASE_URL
+import id.furqoncreative.jetstories.data.source.network.JestoriesNetworkDataSource
 import id.furqoncreative.jetstories.data.source.network.JetstoriesApiService
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -19,4 +21,10 @@ object NetworkDataModules {
     fun provideAppService(): JetstoriesApiService = Retrofit.Builder()
         .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(BASE_URL).build().create(JetstoriesApiService::class.java)
+
+    @Singleton
+    @Provides
+    fun provideNetworkDataSource(
+        appService: JetstoriesApiService,
+    ) = JestoriesNetworkDataSource(appService)
 }
