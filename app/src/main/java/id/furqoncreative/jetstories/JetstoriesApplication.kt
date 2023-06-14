098f6c00062faa4e53e9b55e92357d799d39295c
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-14 16:43:04 +0700

    delete `AppContainer`

--- app/src/main/java/id/furqoncreative/jetstories/JetstoriesApplication.kt
+++ app/src/main/java/id/furqoncreative/jetstories/JetstoriesApplication.kt
@@ -2,16 +2,6 @@ package id.furqoncreative.jetstories
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.android.qualifiers.ApplicationContext
-import id.furqoncreative.jetstories.data.AppContainer
-import id.furqoncreative.jetstories.data.DefaultAppContainer
 
 @HiltAndroidApp
-class JetstoriesApplication : Application() {
-
-    lateinit var container: AppContainer
-    override fun onCreate() {
-        super.onCreate()
-        container = DefaultAppContainer()
-    }
-}
+class JetstoriesApplication : Application()
--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/LoginViewModel.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/LoginViewModel.kt
@@ -5,12 +5,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
-import id.furqoncreative.jetstories.JetstoriesApplication
 import id.furqoncreative.jetstories.data.repository.LoginRepository
 import id.furqoncreative.jetstories.model.login.LoginResponse
 import kotlinx.coroutines.launch
@@ -41,16 +36,6 @@ class LoginViewModel(
             } catch (e: HttpException) {
                 Log.d("TAG", "loginUser: $e")
                 LoginUiState.Error
-            }
-        }
-    }
-
-    companion object {
-        val Factory: ViewModelProvider.Factory = viewModelFactory {
-            initializer {
-                val application = (this[APPLICATION_KEY] as JetstoriesApplication)
-                val loginRepository = application.container.loginRepository
-                LoginViewModel(loginRepository = loginRepository)
             }
         }
     }
