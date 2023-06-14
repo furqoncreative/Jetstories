d50c3930bcb335f04387f97ec55f04f86f90d036
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-14 16:10:37 +0700

    use hilt for application

--- app/src/main/java/id/furqoncreative/jetstories/JetstoriesApplication.kt
+++ app/src/main/java/id/furqoncreative/jetstories/JetstoriesApplication.kt
@@ -1,9 +1,12 @@
 package id.furqoncreative.jetstories
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
 import id.furqoncreative.jetstories.data.AppContainer
 import id.furqoncreative.jetstories.data.DefaultAppContainer
 
+@HiltAndroidApp
 class JetstoriesApplication : Application() {
 
     lateinit var container: AppContainer
--- app/src/main/java/id/furqoncreative/jetstories/MainActivity.kt
+++ app/src/main/java/id/furqoncreative/jetstories/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import id.furqoncreative.jetstories.ui.JetStoriesApp
 import id.furqoncreative.jetstories.ui.theme.JetStoriesTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
