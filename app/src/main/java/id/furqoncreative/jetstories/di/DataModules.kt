dc0a04e858074274c39db3bd65351c8c46e01d7c
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-14 16:58:50 +0700

    bind data source & repository

--- app/src/main/java/id/furqoncreative/jetstories/di/DataModules.kt
+++ app/src/main/java/id/furqoncreative/jetstories/di/DataModules.kt
@@ -1,13 +1,17 @@
 package id.furqoncreative.jetstories.di
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import id.furqoncreative.jetstories.BuildConfig.BASE_URL
+import id.furqoncreative.jetstories.data.repository.LoginRepository
+import id.furqoncreative.jetstories.data.repository.NetworkLoginRepository
 import id.furqoncreative.jetstories.data.source.network.JestoriesNetworkDataSource
 import id.furqoncreative.jetstories.data.source.network.JetstoriesApiService
+import id.furqoncreative.jetstories.data.source.network.NetworkDataSource
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
@@ -27,4 +31,22 @@ object NetworkDataModules {
     fun provideNetworkDataSource(
         appService: JetstoriesApiService,
     ) = JestoriesNetworkDataSource(appService)
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Singleton
+    @Binds
+    abstract fun bindLoginRepository(repository: NetworkLoginRepository): LoginRepository
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+
+    @Singleton
+    @Binds
+    abstract fun bindNetworkDataSource(dataSource: JestoriesNetworkDataSource): NetworkDataSource
 }
