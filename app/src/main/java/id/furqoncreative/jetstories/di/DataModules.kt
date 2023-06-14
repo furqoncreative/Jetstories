00aa1efb8e49826eb2b4c429fcb339c61101e615
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-14 16:25:19 +0700

    create `NetworkDataModules` DI

---
+++ app/src/main/java/id/furqoncreative/jetstories/di/DataModules.kt
@@ -0,0 +1,22 @@
+package id.furqoncreative.jetstories.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import id.furqoncreative.jetstories.BuildConfig.BASE_URL
+import id.furqoncreative.jetstories.data.source.network.response.JetstoriesApiService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkDataModules {
+
+    @Provides
+    fun provideAppService(): JetstoriesApiService = Retrofit.Builder()
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(BASE_URL).build().create(JetstoriesApiService::class.java)
+}
