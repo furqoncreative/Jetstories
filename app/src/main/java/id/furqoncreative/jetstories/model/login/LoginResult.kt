84e7113418e5f318c5c5d76bab5db1c32dbb2c59
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-18 22:53:29 +0700

    make non nullable

--- app/src/main/java/id/furqoncreative/jetstories/model/login/LoginResult.kt
+++ app/src/main/java/id/furqoncreative/jetstories/model/login/LoginResult.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.SerializedName
 
 data class LoginResult(
     @SerializedName("name") val name: String?,
-    @SerializedName("token") val token: String?,
+    @SerializedName("token") val token: String,
     @SerializedName("userId") val userId: String?
 )
