2424132dabaa75c634160cfe58d1cc489b6d2240
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-18 20:44:33 +0700

    update data module injection

--- app/src/main/java/id/furqoncreative/jetstories/di/DataModules.kt
+++ app/src/main/java/id/furqoncreative/jetstories/di/DataModules.kt
@@ -1,20 +1,22 @@
 package id.furqoncreative.jetstories.di
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import id.furqoncreative.jetstories.BuildConfig
 import id.furqoncreative.jetstories.BuildConfig.BASE_URL
 import id.furqoncreative.jetstories.data.repository.LoginRepository
 import id.furqoncreative.jetstories.data.repository.NetworkLoginRepository
 import id.furqoncreative.jetstories.data.source.network.JestoriesNetworkDataSource
 import id.furqoncreative.jetstories.data.source.network.JetstoriesApiService
 import id.furqoncreative.jetstories.data.source.network.NetworkDataSource
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -22,9 +24,21 @@ import javax.inject.Singleton
 object NetworkDataModules {
 
     @Provides
-    fun provideAppService(): JetstoriesApiService = Retrofit.Builder()
-        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(BASE_URL).build().create(JetstoriesApiService::class.java)
+    @Singleton
+    fun okHttpCallFactory(): Call.Factory = OkHttpClient.Builder().addInterceptor(
+            HttpLoggingInterceptor().apply {
+                    if (BuildConfig.DEBUG) {
+                        setLevel(HttpLoggingInterceptor.Level.BODY)
+                    }
+                },
+        ).build()
+
+    @Provides
+    fun provideAppService(
+        okhttpCallFactory: Call.Factory
+    ): JetstoriesApiService = Retrofit.Builder().callFactory(okhttpCallFactory)
+        .addConverterFactory(GsonConverterFactory.create()).baseUrl(BASE_URL).build()
+        .create(JetstoriesApiService::class.java)
 
     @Singleton
     @Provides
