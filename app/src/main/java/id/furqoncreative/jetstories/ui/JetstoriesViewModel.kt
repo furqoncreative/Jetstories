1384effd4c94dd3a40978065361dd0985f769775
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-18 23:02:35 +0700

    create JetstoriesViewModel

---
+++ app/src/main/java/id/furqoncreative/jetstories/ui/JetstoriesViewModel.kt
@@ -0,0 +1,34 @@
+package id.furqoncreative.jetstories.ui
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import id.furqoncreative.jetstories.data.source.local.PreferencesManager
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class JetstoriesViewModel @Inject constructor(
+    private val preferencesManager: PreferencesManager
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(false)
+    val isLoggedIn: StateFlow<Boolean> = _uiState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            checkUserLoginStatus()
+        }
+    }
+
+    private suspend fun checkUserLoginStatus() = preferencesManager.getUserToken.collect { token ->
+        Log.d("TAG", "checkUserLoginStatus: $token")
+        if (token.isNotEmpty()) {
+            _uiState.update { true }
+        }
+    }
+}
