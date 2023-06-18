177c2b9429777043686f4e3f8b96678ad559ef48
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-18 21:15:33 +0700

    remove scaffold

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/LoginScreen.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/login/LoginScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHost
