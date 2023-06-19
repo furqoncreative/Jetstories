e9789919b489ff083744fb23c4b2d1f905f6c125
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-19 14:03:41 +0700

    change `getAllStories` data source

--- app/src/main/java/id/furqoncreative/jetstories/data/source/network/NetworkDataSource.kt
+++ app/src/main/java/id/furqoncreative/jetstories/data/source/network/NetworkDataSource.kt
@@ -1,10 +1,15 @@
 package id.furqoncreative.jetstories.data.source.network
 
 import id.furqoncreative.jetstories.model.login.LoginResponse
+import id.furqoncreative.jetstories.model.stories.GetAllStoriesResponse
 import kotlinx.coroutines.flow.Flow
 
 interface NetworkDataSource {
     suspend fun loginUser(
         email: String, password: String
     ): Flow<LoginResponse>
+
+    suspend fun getAllStories(
+        page: Int?, size: Int?, location: Int?
+    ): Flow<GetAllStoriesResponse>
 }
