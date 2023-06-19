fa25313ce864693ca771f25656214d5bfa7f52da
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-19 14:00:24 +0700

    rename data class

--- app/src/main/java/id/furqoncreative/jetstories/model/stories/GetAllStoriesResponse.kt
+++ app/src/main/java/id/furqoncreative/jetstories/model/stories/GetAllStoriesResponse.kt
@@ -3,7 +3,7 @@ package id.furqoncreative.jetstories.model.stories
 
 import com.google.gson.annotations.SerializedName
 
-data class GetAllStories(
+data class GetAllStoriesResponse(
     @SerializedName("error") val error: Boolean?,
     @SerializedName("listStory") val listStory: List<Story?>?,
     @SerializedName("message") val message: String?
