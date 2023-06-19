51ea721f33785d64e89ebb3d04b9d49bc3371c2e
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-19 13:59:33 +0700

    add `GetAllStories` api service

--- app/src/main/java/id/furqoncreative/jetstories/data/source/network/JetstoriesApiService.kt
+++ app/src/main/java/id/furqoncreative/jetstories/data/source/network/JetstoriesApiService.kt
@@ -4,6 +4,7 @@ import id.furqoncreative.jetstories.model.login.LoginResponse
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface JetstoriesApiService {
 
@@ -11,5 +12,12 @@ interface JetstoriesApiService {
     @POST("login")
     suspend fun loginUser(
         @Field("email") email: String, @Field("password") password: String
+    ): LoginResponse
+
+    @POST("login")
+    suspend fun getAllStories(
+        @Query("page") page: Int?,
+        @Query("size") size: Int?,
+        @Query("location") location: Int? = 0,
     ): LoginResponse
 }
