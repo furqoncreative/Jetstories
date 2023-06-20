e26a777b4d027cc95b56bb0d744f37d15fe9cad1
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-20 13:25:50 +0700

    use Timber library

--- app/src/main/java/id/furqoncreative/jetstories/JetstoriesApplication.kt
+++ app/src/main/java/id/furqoncreative/jetstories/JetstoriesApplication.kt
@@ -2,6 +2,12 @@ package id.furqoncreative.jetstories
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class JetstoriesApplication : Application()
+class JetstoriesApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
