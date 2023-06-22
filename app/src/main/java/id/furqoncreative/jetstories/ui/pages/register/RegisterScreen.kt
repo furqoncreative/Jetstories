512549e45fdadc2dcd44f835e43bff4ff68bdebe
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-22 16:50:32 +0700

    create register screen

---
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/register/RegisterScreen.kt
@@ -0,0 +1,9 @@
+package id.furqoncreative.jetstories.ui.pages.register
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegisterScreen() {
+    Text(text = "Register")
+}
