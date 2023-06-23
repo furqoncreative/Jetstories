f38b43d69d2ca235eef04959409abf522f876206
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-23 14:39:49 +0700

    create register response model

---
+++ app/src/main/java/id/furqoncreative/jetstories/model/register/RegisterResponse.kt
@@ -0,0 +1,11 @@
+package id.furqoncreative.jetstories.model.register
+
+
+import com.google.gson.annotations.SerializedName
+
+data class RegisterResponse(
+    @SerializedName("error")
+    val error: Boolean?,
+    @SerializedName("message")
+    val message: String?
+)
