b2f483b1faf903d6f03cd61ac71038600f82a867
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-24 21:33:45 +0700

    create add story response

---
+++ app/src/main/java/id/furqoncreative/jetstories/model/stories/AddStoryResponse.kt
@@ -0,0 +1,8 @@
+package id.furqoncreative.jetstories.model.stories
+
+
+import com.google.gson.annotations.SerializedName
+
+data class AddStoryResponse(
+    @SerializedName("error") val error: Boolean?, @SerializedName("message") val message: String?
+)
