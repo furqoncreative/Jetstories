2574e982e7feb44c791ebfe97efc0f7495109c5c
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-24 22:16:37 +0700

    create add story view model

---
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/addstory/AddStoryViewModel.kt
@@ -0,0 +1,7 @@
+package id.furqoncreative.jetstories.ui.pages.addstory
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class AddStoryViewModel : ViewModel()
