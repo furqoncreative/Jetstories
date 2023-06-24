a166a7ad2a98b935ba06c7060d6703db2a41214c
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-24 19:28:10 +0700

    create get detail story repository

---
+++ app/src/main/java/id/furqoncreative/jetstories/data/repository/GetDetailRepository.kt
@@ -0,0 +1,33 @@
+package id.furqoncreative.jetstories.data.repository
+
+import id.furqoncreative.jetstories.data.source.local.PreferencesManager
+import id.furqoncreative.jetstories.data.source.network.NetworkDataSource
+import id.furqoncreative.jetstories.model.stories.GetDetailStoryResponse
+import id.furqoncreative.jetstories.utils.Async
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+
+interface GetDetailStoryRepository {
+    suspend fun getDetailStory(
+        id: String
+    ): Flow<Async<GetDetailStoryResponse>>
+}
+
+class NetworkGetDetailStoryRepository(
+    private val preferencesManager: PreferencesManager,
+    private val networkDataSource: NetworkDataSource
+) : GetDetailStoryRepository {
+    override suspend fun getDetailStory(id: String): Flow<Async<GetDetailStoryResponse>> {
+        val userToken = "Bearer ${preferencesManager.getUserToken.first()}"
+
+        return networkDataSource.getDetailStory(
+            token = userToken, id = id
+        ).map {
+            Async.Success(it)
+        }.catch<Async<GetDetailStoryResponse>> {
+            Async.Error(it.message)
+        }
+    }
+}
