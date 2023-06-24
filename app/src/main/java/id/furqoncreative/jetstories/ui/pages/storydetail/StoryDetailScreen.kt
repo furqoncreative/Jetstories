f0d6572dd0ddec2a1f3c0e166d06aefcf36fd877
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-24 19:13:55 +0700

    create story detail ui data state

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/storydetail/StoryDetailScreen.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/storydetail/StoryDetailScreen.kt
@@ -1,7 +1,15 @@
 package id.furqoncreative.jetstories.ui.pages.storydetail
 
 import androidx.compose.runtime.Composable
+import id.furqoncreative.jetstories.model.stories.Story
 
+data class StoryDetailUiState(
+    val isEmpty: Boolean = false,
+    val isLoading: Boolean = false,
+    val isUserLogout: Boolean = false,
+    val userMessage: String? = null,
+    val stories: Story? = null,
+)
 @Composable
 fun StoryDetailScreen() {
     
