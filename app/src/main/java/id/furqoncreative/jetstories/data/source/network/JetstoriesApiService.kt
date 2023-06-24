cf845ed527a541d7a3d012ffd126fbb091fdb670
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-24 21:40:53 +0700

    make nullable

--- app/src/main/java/id/furqoncreative/jetstories/data/source/network/JetstoriesApiService.kt
+++ app/src/main/java/id/furqoncreative/jetstories/data/source/network/JetstoriesApiService.kt
@@ -4,11 +4,14 @@ import id.furqoncreative.jetstories.model.login.LoginResponse
 import id.furqoncreative.jetstories.model.register.RegisterResponse
 import id.furqoncreative.jetstories.model.stories.GetAllStoriesResponse
 import id.furqoncreative.jetstories.model.stories.GetDetailStoryResponse
+import okhttp3.MultipartBody
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -27,6 +30,16 @@ interface JetstoriesApiService {
         @Field("name") name: String,
         @Field("password") password: String
     ): RegisterResponse
+
+    @Multipart
+    @GET("stories")
+    suspend fun addStory(
+        @Header("Authorization") token: String,
+        @Part file: MultipartBody.Part,
+        @Query("description") description: String,
+        @Query("lat") latitude: Float?,
+        @Query("lon") longitude: Float?,
+    ): GetAllStoriesResponse
 
     @GET("stories")
     suspend fun getAllStories(
