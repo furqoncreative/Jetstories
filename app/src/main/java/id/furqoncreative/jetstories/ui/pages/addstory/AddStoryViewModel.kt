6d280e44029839c1a715ec0d0b4cc9e7121a78d2
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-25 21:21:36 +0700

    create add story ui state

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/addstory/AddStoryViewModel.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/addstory/AddStoryViewModel.kt
@@ -2,6 +2,15 @@ package id.furqoncreative.jetstories.ui.pages.addstory
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import id.furqoncreative.jetstories.utils.DescriptionState
+
+
+data class AddStoryUiState(
+    val descriptionState: DescriptionState = DescriptionState(),
+    val isLoading: Boolean = false,
+    val userMessage: String? = null,
+    val isSuccessAddStory: Boolean = false
+)
 
 @HiltViewModel
 class AddStoryViewModel : ViewModel()
