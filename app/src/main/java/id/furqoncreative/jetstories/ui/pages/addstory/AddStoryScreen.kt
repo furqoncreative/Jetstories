dec060d2c0823104d3b7d507f9b4fc129ad94fe5
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-25 20:50:27 +0700

    create basic layout add story

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/addstory/AddStoryScreen.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/addstory/AddStoryScreen.kt
@@ -1,2 +1,167 @@
 package id.furqoncreative.jetstories.ui.pages.addstory
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.FileUpload
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import id.furqoncreative.jetstories.R
+import id.furqoncreative.jetstories.ui.components.JetstoriesProgressBar
+import id.furqoncreative.jetstories.ui.theme.JetStoriesTheme
+import me.onebone.toolbar.CollapsingToolbarScaffold
+import me.onebone.toolbar.CollapsingToolbarScaffoldState
+import me.onebone.toolbar.ScrollStrategy
+import me.onebone.toolbar.rememberCollapsingToolbarScaffoldState
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddStoryScreen(
+    modifier: Modifier = Modifier,
+    onNavUp: () -> Unit,
+    collapsingToolbarScaffoldState: CollapsingToolbarScaffoldState = rememberCollapsingToolbarScaffoldState(),
+) {
+
+    CollapsingToolbarScaffold(modifier = modifier,
+        state = collapsingToolbarScaffoldState,
+        scrollStrategy = ScrollStrategy.ExitUntilCollapsed,
+        toolbar = {
+            val textSize =
+                (20 + (30 - 12) * collapsingToolbarScaffoldState.toolbarState.progress).sp
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .pin()
+                    .background(color = MaterialTheme.colorScheme.background)
+            )
+            Text(
+                text = "Add Story", textAlign = TextAlign.Center, style = TextStyle(
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontSize = textSize,
+                    fontWeight = FontWeight.Medium
+                ), modifier = Modifier
+                    .padding(
+                        top = 10.dp, start = 40.dp, bottom = 16.dp, end = 40.dp
+                    )
+                    .road(whenCollapsed = Alignment.TopStart, whenExpanded = Alignment.Center)
+            )
+            Row(
+                modifier = Modifier.road(
+                    whenExpanded = Alignment.BottomStart, whenCollapsed = Alignment.TopStart
+                )
+            ) {
+                IconButton(onClick = {
+                    onNavUp()
+                }) {
+                    Icon(imageVector = Icons.Default.ChevronLeft, contentDescription = "Back")
+                }
+            }
+        }) {
+
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .border(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = RoundedCornerShape(16.dp)
+                    ), contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    modifier = Modifier.size(150.dp), painter = painterResource(
+                        id = R.drawable.img_add_story_placeholder
+                    ), contentDescription = "Add Story"
+                )
+
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(bottom = 16.dp)
+                        .clickable {
+
+                        },
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.Default.FileUpload,
+                        contentDescription = "Upload",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Text(
+                        style = MaterialTheme.typography.labelLarge,
+                        text = "ADD MEDIA",
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+            }
+
+            TextField(modifier = Modifier.fillMaxWidth(), value = "", onValueChange = {}, label = {
+                Text(text = "Description")
+            }, placeholder = {
+                Text("Tell your story...")
+            }, trailingIcon = {
+                Icon(Icons.Default.Clear, contentDescription = "Clear")
+            }, maxLines = 3)
+
+            Button(modifier = Modifier
+                .height(56.dp)
+                .fillMaxWidth(), enabled = true, onClick = {}) {
+                if (!false) {
+                    Text(text = "Submit")
+                } else {
+                    JetstoriesProgressBar(size = 30.dp)
+                }
+            }
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun AddStoryScreenPreview() {
+    JetStoriesTheme {
+        AddStoryScreen(onNavUp = { /*TODO*/ })
+    }
+}
