5d5dfa2fe6bf4efdc015c454131f8d2af1e268d1
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-27 06:56:17 +0700

    make message no nullable

--- app/src/main/java/id/furqoncreative/jetstories/utils/Async.kt
+++ app/src/main/java/id/furqoncreative/jetstories/utils/Async.kt
@@ -3,7 +3,7 @@ package id.furqoncreative.jetstories.utils
 sealed class Async<out T> {
     object Loading : Async<Nothing>()
 
-    data class Error(val errorMessage: String?) : Async<Nothing>()
+    data class Error(val errorMessage: String) : Async<Nothing>()
 
     data class Success<out T>(val data: T) : Async<T>()
 }
