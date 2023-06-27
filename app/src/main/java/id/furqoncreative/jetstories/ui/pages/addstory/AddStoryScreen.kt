0cb80d8986b633501979c1a780c13b282630b80a
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-27 14:53:26 +0700

    add story clean up

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/addstory/AddStoryScreen.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/addstory/AddStoryScreen.kt
@@ -1,31 +1,16 @@
 package id.furqoncreative.jetstories.ui.pages.addstory
 
-import android.Manifest
 import android.net.Uri
-import android.os.Build
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChevronLeft
-import androidx.compose.material.icons.filled.FileUpload
-import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -36,10 +21,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -49,10 +32,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import coil.compose.rememberAsyncImagePainter
 import id.furqoncreative.jetstories.R
-import id.furqoncreative.jetstories.ui.components.JetstoriesDescriptionTextField
-import id.furqoncreative.jetstories.ui.components.JetstoriesProgressBar
 import id.furqoncreative.jetstories.ui.theme.JetStoriesTheme
 import id.furqoncreative.jetstories.utils.SquireCropImage
 import id.furqoncreative.jetstories.utils.showToast
@@ -69,10 +49,11 @@ fun AddStoryScreen(
     modifier: Modifier = Modifier,
     onNavUp: () -> Unit,
     onSuccessAddStory: () -> Unit,
-    collapsingToolbarScaffoldState: CollapsingToolbarScaffoldState = rememberCollapsingToolbarScaffoldState(),
     addStoryViewModel: AddStoryViewModel = hiltViewModel()
 ) {
     val uiState by addStoryViewModel.uiState.collectAsStateWithLifecycle()
+    val collapsingToolbarScaffoldState: CollapsingToolbarScaffoldState =
+        rememberCollapsingToolbarScaffoldState()
     val context = LocalContext.current
     val keyboardController = LocalSoftwareKeyboardController.current
 
@@ -170,88 +151,14 @@ fun AddStoryScreen(
             }
         }) {
 
-        Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxHeight()
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(300.dp)
-                    .border(
-                        width = 1.dp,
-                        color = MaterialTheme.colorScheme.primary,
-                        shape = RoundedCornerShape(16.dp)
-                    ), contentAlignment = Alignment.Center
-            ) {
-                if (uiState.imageUri != null) {
-                    Image(
-                        modifier = Modifier.fillMaxSize(), painter = rememberAsyncImagePainter(
-                            model = uiState.imageUri
-                        ), contentDescription = stringResource(id = R.string.add_story)
-                    )
-                } else {
-                    Image(
-                        modifier = Modifier.size(150.dp), painter = painterResource(
-                            id = R.drawable.img_add_story_placeholder
-                        ), contentDescription = stringResource(id = R.string.add_story)
-                    )
-                }
-
-                Row(
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter)
-                        .padding(bottom = 16.dp)
-                        .background(MaterialTheme.colorScheme.background)
-                        .clip(RoundedCornerShape(16.dp))
-                        .clickable {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                                requestStoragePermission.launch(Manifest.permission.READ_MEDIA_IMAGES)
-                            } else {
-                                requestStoragePermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
-                            }
-                        },
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        Icons.Default.FileUpload,
-                        contentDescription = stringResource(R.string.add_image),
-                        tint = MaterialTheme.colorScheme.primary
-                    )
-
-                    Text(
-                        style = MaterialTheme.typography.labelLarge,
-                        text = if (uiState.imageUri == null) stringResource(id = R.string.add_image) else stringResource(
-                            R.string.replace_media
-                        ),
-                        color = MaterialTheme.colorScheme.primary
-                    )
-                }
-            }
-
-            JetstoriesDescriptionTextField(context = context, onImeAction = {
+        AddStoryContent(context = context,
+            imageUri = uiState.imageUri,
+            isLoading = uiState.isLoading,
+            descriptionState = uiState.descriptionState,
+            requestStoragePermission = requestStoragePermission,
+            onSubmit = {
                 onSubmit()
-            }, descriptionState = uiState.descriptionState)
-
-            Button(modifier = Modifier
-                .height(56.dp)
-                .fillMaxWidth(),
-                enabled = uiState.descriptionState.isValid && uiState.imageUri != null,
-                onClick = {
-                    onSubmit()
-                }) {
-                if (!uiState.isLoading) {
-                    Text(text = stringResource(R.string.submit))
-                } else {
-                    JetstoriesProgressBar(size = 30.dp)
-                }
-            }
-        }
-
+            })
     }
 }
 
