c2dcbd4de8e17c836373cf653190d23d6c984476
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-30 15:56:25 +0700

    refactor utils

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/addstory/AddStoryViewModel.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/addstory/AddStoryViewModel.kt
@@ -11,7 +11,7 @@ import id.furqoncreative.jetstories.model.stories.AddStoryResponse
 import id.furqoncreative.jetstories.utils.Async
 import id.furqoncreative.jetstories.utils.DescriptionState
 import id.furqoncreative.jetstories.utils.UiText
-import id.furqoncreative.jetstories.utils.uriToFile
+import id.furqoncreative.jetstories.utils.toFile
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -45,7 +45,7 @@ class AddStoryViewModel @Inject constructor(
         viewModelScope.launch {
             val imageUri = uiState.value.imageUri
             if (imageUri != null) {
-                val file: File = uriToFile(imageUri, context)
+                val file: File = imageUri.toFile(context)
                 val description =
                     uiState.value.descriptionState.text.toRequestBody("text/plain".toMediaType())
                 val requestImageFile = file.asRequestBody("image/jpeg".toMediaType())
--- app/src/main/java/id/furqoncreative/jetstories/utils/Utils.kt
+++ app/src/main/java/id/furqoncreative/jetstories/utils/Utils.kt
@@ -2,6 +2,8 @@ package id.furqoncreative.jetstories.utils
 
 import android.content.ContentResolver
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Environment
 import android.widget.Toast
@@ -9,6 +11,7 @@ import androidx.core.content.FileProvider
 import id.furqoncreative.jetstories.BuildConfig
 import id.furqoncreative.jetstories.R
 import id.furqoncreative.jetstories.ui.components.MenuItem
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
@@ -19,13 +22,14 @@ import java.util.Locale
 
 
 private const val FILENAME_FORMAT = "dd-MMM-yyyy"
+private const val MAXIMAL_SIZE = 1000000
 
 val timeStamp: String = SimpleDateFormat(
     FILENAME_FORMAT, Locale.US
 ).format(System.currentTimeMillis())
 
-fun createCustomTempFile(context: Context): File {
-    val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+fun Context.createCustomTempFile(): File {
+    val storageDir: File? = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
     return File.createTempFile(timeStamp, ".jpg", storageDir)
 }
 
@@ -33,9 +37,7 @@ fun Context.createImageFile(): File {
     val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(Date())
     val storageDir = cacheDir
     return File.createTempFile(
-        "JPEG_${timeStamp}_",
-        ".jpg",
-        storageDir
+        "JPEG_${timeStamp}_", ".jpg", storageDir
     ).apply {
         deleteOnExit()
     }
@@ -43,17 +45,31 @@ fun Context.createImageFile(): File {
 
 fun File.getUriForFile(context: Context): Uri {
     return FileProvider.getUriForFile(
-        context,
-        "${BuildConfig.APPLICATION_ID}.fileprovider",
-        this
+        context, "${BuildConfig.APPLICATION_ID}.fileprovider", this
     )
 }
 
-fun uriToFile(selectedImg: Uri, context: Context): File {
-    val contentResolver: ContentResolver = context.contentResolver
-    val myFile = createCustomTempFile(context)
+fun File.reduceFileImage(): File {
+    val file = this
+    val bitmap = BitmapFactory.decodeFile(file.path)
+    var compressQuality = 100
+    var streamLength: Int
+    do {
+        val bmpStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream)
+        val bmpPicByteArray = bmpStream.toByteArray()
+        streamLength = bmpPicByteArray.size
+        compressQuality -= 5
+    } while (streamLength > MAXIMAL_SIZE)
+    bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, FileOutputStream(file))
+    return file
+}
 
-    val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+fun Uri.toFile(context: Context): File {
+    val contentResolver: ContentResolver = context.contentResolver
+    val myFile = context.createCustomTempFile()
+
+    val inputStream = contentResolver.openInputStream(this) as InputStream
     val outputStream: OutputStream = FileOutputStream(myFile)
     val buf = ByteArray(1024)
     var len: Int
