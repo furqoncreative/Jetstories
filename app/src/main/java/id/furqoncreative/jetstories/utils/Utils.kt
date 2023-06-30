d0bad4b03253b0fec46da344ec338f78a09a988d
Deden Muhamad Furqon <akunkhusus42@gmail.com>
2023-06-30 15:51:50 +0700

    add camera utils

--- app/src/main/java/id/furqoncreative/jetstories/utils/Utils.kt
+++ app/src/main/java/id/furqoncreative/jetstories/utils/Utils.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.net.Uri
 import android.os.Environment
 import android.widget.Toast
+import androidx.core.content.FileProvider
+import id.furqoncreative.jetstories.BuildConfig
 import id.furqoncreative.jetstories.R
 import id.furqoncreative.jetstories.ui.components.MenuItem
 import java.io.File
@@ -12,6 +14,7 @@ import java.io.FileOutputStream
 import java.io.InputStream
 import java.io.OutputStream
 import java.text.SimpleDateFormat
+import java.util.Date
 import java.util.Locale
 
 
@@ -24,6 +27,26 @@ val timeStamp: String = SimpleDateFormat(
 fun createCustomTempFile(context: Context): File {
     val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
     return File.createTempFile(timeStamp, ".jpg", storageDir)
+}
+
+fun Context.createImageFile(): File {
+    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(Date())
+    val storageDir = cacheDir
+    return File.createTempFile(
+        "JPEG_${timeStamp}_",
+        ".jpg",
+        storageDir
+    ).apply {
+        deleteOnExit()
+    }
+}
+
+fun File.getUriForFile(context: Context): Uri {
+    return FileProvider.getUriForFile(
+        context,
+        "${BuildConfig.APPLICATION_ID}.fileprovider",
+        this
+    )
 }
 
 fun uriToFile(selectedImg: Uri, context: Context): File {
