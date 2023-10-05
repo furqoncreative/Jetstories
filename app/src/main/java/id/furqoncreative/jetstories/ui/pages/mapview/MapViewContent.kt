d6727047e106c509e262819f0e216da66c180806
Deden Muhamad Furqon <furqoncreative24@gmail.com>
2023-10-05 15:39:12 +0700

    Extract Maps view content

---
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/mapview/MapViewContent.kt
@@ -0,0 +1,34 @@
+package id.furqoncreative.jetstories.ui.pages.mapview
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import id.furqoncreative.jetstories.model.stories.Story
+
+@Composable
+fun MapViewContent(
+    modifier: Modifier = Modifier,
+    isLoading: Boolean,
+    stories: List<Story>?,
+    onClickStory: (Story) -> Unit
+) {
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(), cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = MarkerState(position = singapore),
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
+    }
+}
