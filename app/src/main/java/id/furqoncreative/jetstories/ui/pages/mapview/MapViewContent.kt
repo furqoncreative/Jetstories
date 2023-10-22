5efad5b9ca2ccffe7cb77c73e0ec08dc70f00227
Deden Muhamad Furqon <furqoncreative24@gmail.com>
2023-10-22 16:32:02 +0700

    Add loading indicator when map is loaded

--- app/src/main/java/id/furqoncreative/jetstories/ui/pages/mapview/MapViewContent.kt
+++ app/src/main/java/id/furqoncreative/jetstories/ui/pages/mapview/MapViewContent.kt
@@ -1,11 +1,17 @@
 package id.furqoncreative.jetstories.ui.pages.mapview
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,7 +44,6 @@ import kotlinx.coroutines.launch
 @Composable
 fun MapViewContent(
     modifier: Modifier = Modifier,
-    isLoading: Boolean,
     isMyLocationEnabled: Boolean,
     stories: List<Story>?,
     selectedStory: Story?,
@@ -46,6 +52,7 @@ fun MapViewContent(
     clearSelectedStory: () -> Unit
 ) {
     val scope = rememberCoroutineScope()
+    var isMapLoaded by remember { mutableStateOf(false) }
 
     val uiSettings by remember {
         mutableStateOf(
@@ -72,70 +79,91 @@ fun MapViewContent(
         position = CameraPosition.fromLatLngZoom(LatLng(-6.905977, 107.613144), 7.5F)
     }
 
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState,
-        googleMapOptionsFactory = {
-            GoogleMapOptions()
-        },
-        properties = properties,
-        uiSettings = uiSettings,
-    ) {
+    Box(modifier.fillMaxSize()) {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState,
+            googleMapOptionsFactory = {
+                GoogleMapOptions()
+            },
+            properties = properties,
+            uiSettings = uiSettings,
+            onMapLoaded = {
+                isMapLoaded = true
+            }
+        ) {
 
-        if (!stories.isNullOrEmpty()) {
-            for (story in stories) {
-                val location = LatLng(story.lat ?: 0.0, story.lon ?: 0.0)
+            if (!stories.isNullOrEmpty()) {
+                for (story in stories) {
+                    val location = LatLng(story.lat ?: 0.0, story.lon ?: 0.0)
 
-                val markerState by remember {
-                    mutableStateOf(MarkerState(location))
-                }
+                    val markerState by remember {
+                        mutableStateOf(MarkerState(location))
+                    }
 
-                MarkerInfoWindow(state = markerState,
-                    anchor = if (selectedStory == story) {
-                        Offset(.5f, .7f)
-                    } else {
-                        Offset(.5f, 1f)
-                    },
-                    title = story.name,
-                    snippet = "Click for details",
-                    icon = if (selectedStory == story) {
-                        BitmapDescriptorFactory.defaultMarker(100F)
-                    } else {
-                        BitmapDescriptorFactory.defaultMarker()
-                    },
-                    onClick = {
-                        setSelectedStory(story)
-                        false
-                    },
-                    onInfoWindowClick = {
-                        scope.launch {
-                            onClickStory(story)
-                        }
-                    },
-                    onInfoWindowClose = {
-                        clearSelectedStory()
-                    }) { marker ->
+                    MarkerInfoWindow(state = markerState,
+                        anchor = if (selectedStory == story) {
+                            Offset(.5f, .7f)
+                        } else {
+                            Offset(.5f, 1f)
+                        },
+                        title = story.name,
+                        snippet = "Click for details",
+                        icon = if (selectedStory == story) {
+                            BitmapDescriptorFactory.defaultMarker(100F)
+                        } else {
+                            BitmapDescriptorFactory.defaultMarker()
+                        },
+                        onClick = {
+                            setSelectedStory(story)
+                            false
+                        },
+                        onInfoWindowClick = {
+                            scope.launch {
+                                onClickStory(story)
+                            }
+                        },
+                        onInfoWindowClose = {
+                            clearSelectedStory()
+                        }) { marker ->
 
-                    Column(
-                        Modifier
-                            .clip(RoundedCornerShape(8.dp))
-                            .border(
-                                1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(8.dp)
+                        Column(
+                            Modifier
+                                .clip(RoundedCornerShape(8.dp))
+                                .border(
+                                    1.dp,
+                                    MaterialTheme.colorScheme.primary,
+                                    RoundedCornerShape(8.dp)
+                                )
+                                .background(MaterialTheme.colorScheme.background)
+                                .padding(8.dp, 2.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = marker.title ?: "Name",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold
                             )
-                            .background(MaterialTheme.colorScheme.background)
-                            .padding(8.dp, 2.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text = marker.title ?: "Name",
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                        Text(
-                            text = marker.snippet ?: "Story", fontSize = 14.sp
-                        )
+                            Text(
+                                text = marker.snippet ?: "Story", fontSize = 14.sp
+                            )
+                        }
                     }
                 }
+            }
+        }
+        if (!isMapLoaded) {
+            AnimatedVisibility(
+                modifier = Modifier.matchParentSize(),
+                visible = !isMapLoaded,
+                enter = EnterTransition.None,
+                exit = fadeOut()
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background)
+                        .wrapContentSize()
+                )
             }
         }
     }
