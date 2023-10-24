45eb890a2b6289d29ee73aba53b2eda360ab3764
Deden Muhamad Furqon <furqoncreative24@gmail.com>
2023-10-24 09:15:48 +0700

    Inject database & repository

--- app/src/main/java/id/furqoncreative/jetstories/di/DataModules.kt
+++ app/src/main/java/id/furqoncreative/jetstories/di/DataModules.kt
@@ -14,15 +14,18 @@ import id.furqoncreative.jetstories.BuildConfig
 import id.furqoncreative.jetstories.BuildConfig.BASE_URL
 import id.furqoncreative.jetstories.data.repository.AddStoryRepository
 import id.furqoncreative.jetstories.data.repository.GetAllStoriesRepository
+import id.furqoncreative.jetstories.data.repository.GetAllStoriesWithPaginationRepository
 import id.furqoncreative.jetstories.data.repository.GetDetailStoryRepository
 import id.furqoncreative.jetstories.data.repository.LoginRepository
 import id.furqoncreative.jetstories.data.repository.NetworkAddStoryRepository
 import id.furqoncreative.jetstories.data.repository.NetworkGetAllStoriesRepository
+import id.furqoncreative.jetstories.data.repository.NetworkGetAllStoriesWithPaginationRepository
 import id.furqoncreative.jetstories.data.repository.NetworkGetDetailStoryRepository
 import id.furqoncreative.jetstories.data.repository.NetworkLoginRepository
 import id.furqoncreative.jetstories.data.repository.NetworkRegisterRepository
 import id.furqoncreative.jetstories.data.repository.RegisterRepository
 import id.furqoncreative.jetstories.data.source.local.PreferencesManager
+import id.furqoncreative.jetstories.data.source.local.StoryDatabase
 import id.furqoncreative.jetstories.data.source.network.JestoriesNetworkDataSource
 import id.furqoncreative.jetstories.data.source.network.JetstoriesApiService
 import id.furqoncreative.jetstories.data.source.network.NetworkDataSource
@@ -35,12 +38,16 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DataStorePreferencesModule {
+object LocalDataModule {
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "jetstories-preferences")
 
     @Singleton
     @Provides
     fun provideDataStore(@ApplicationContext app: Context): DataStore<Preferences> = app.dataStore
+
+    @Singleton
+    @Provides
+    fun provideDataBase(@ApplicationContext context: Context): StoryDatabase = StoryDatabase.getDatabase(context)
 
     @Singleton
     @Provides
@@ -91,6 +98,10 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun bindGetAllStoryRepository(repository: NetworkGetAllStoriesRepository): GetAllStoriesRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindGetAllStoryWithPaginationRepository(repository: NetworkGetAllStoriesWithPaginationRepository): GetAllStoriesWithPaginationRepository
 
     @Singleton
     @Binds
