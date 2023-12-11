cff542fe514344159a17d4d07d80cb1eeaa7e693
Deden Muhamad Furqon <furqoncreative24@gmail.com>
2023-12-11 17:00:38 +0700

    Use base test

--- app/src/androidTest/java/id/furqoncreative/jetstories/ui/screens/home/HomeScreenKtTest.kt
+++ app/src/androidTest/java/id/furqoncreative/jetstories/ui/screens/home/HomeScreenKtTest.kt
@@ -1,53 +1,33 @@
 package id.furqoncreative.jetstories.ui.screens.home
 
-import androidx.activity.compose.setContent
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.navigation.compose.ComposeNavigator
-import androidx.navigation.testing.TestNavHostController
-import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import id.furqoncreative.jetstories.JetstoriesActivity
+import id.furqoncreative.jetstories.JetstoriesScreenTest
 import id.furqoncreative.jetstories.assertCurrentRouteName
 import id.furqoncreative.jetstories.ui.navigation.JetstoriesNavGraph
 import id.furqoncreative.jetstories.ui.navigation.JetstoriesScreens
 import id.furqoncreative.jetstories.ui.navigation.JetstoriesScreens.HOME_SCREEN
-import id.furqoncreative.jetstories.ui.theme.JetStoriesTheme
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
 @HiltAndroidTest
-class HomeScreenKtTest {
-    @get:Rule(order = 0)
-    val hiltRule = HiltAndroidRule(this)
-
-    @get:Rule(order = 1)
-    val composeTestRule = createAndroidComposeRule<JetstoriesActivity>()
-
-    private lateinit var navHostController: TestNavHostController
+class HomeScreenKtTest : JetstoriesScreenTest() {
 
     @Before
     fun setUp() {
-        hiltRule.inject()
-        composeTestRule.activity.setContent {
-            JetStoriesTheme {
-                navHostController = TestNavHostController(LocalContext.current)
-                navHostController.navigatorProvider.addNavigator(ComposeNavigator())
-                JetstoriesNavGraph(
-                    navController = navHostController,
-                    startDestination = HOME_SCREEN
-                )
-            }
+        inject()
+        setJetstoriesContent {
+            JetstoriesNavGraph(
+                navController = navHostController,
+                startDestination = HOME_SCREEN
+            )
         }
     }
 
     @Test
     fun navigateToLoginScreen_onSuccessfulLogout() {
-
         composeTestRule.apply {
             onNodeWithContentDescription("More Menu").performClick()
             onNodeWithText("Logout").performClick()
--- app/src/androidTest/java/id/furqoncreative/jetstories/ui/screens/login/LoginScreenKtTest.kt
+++ app/src/androidTest/java/id/furqoncreative/jetstories/ui/screens/login/LoginScreenKtTest.kt
@@ -1,43 +1,27 @@
 package id.furqoncreative.jetstories.ui.screens.login
 
-import androidx.activity.compose.setContent
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextReplacement
-import androidx.navigation.compose.ComposeNavigator
-import androidx.navigation.testing.TestNavHostController
-import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import id.furqoncreative.jetstories.JetstoriesActivity
+import id.furqoncreative.jetstories.JetstoriesScreenTest
 import id.furqoncreative.jetstories.assertCurrentRouteName
 import id.furqoncreative.jetstories.ui.navigation.JetstoriesNavGraph
 import id.furqoncreative.jetstories.ui.navigation.JetstoriesScreens
-import id.furqoncreative.jetstories.ui.theme.JetStoriesTheme
+import id.furqoncreative.jetstories.ui.navigation.JetstoriesScreens.LOGIN_SCREEN
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 
 @HiltAndroidTest
-class LoginScreenKtTest {
-    @get:Rule(order = 0)
-    val hiltRule = HiltAndroidRule(this)
-
-    @get:Rule(order = 1)
-    val composeTestRule = createAndroidComposeRule<JetstoriesActivity>()
-
-    private lateinit var navHostController: TestNavHostController
-
+class LoginScreenKtTest : JetstoriesScreenTest() {
     @Before
     fun setUp() {
-        hiltRule.inject()
-        composeTestRule.activity.setContent {
-            JetStoriesTheme {
-                navHostController = TestNavHostController(LocalContext.current)
-                navHostController.navigatorProvider.addNavigator(ComposeNavigator())
-                JetstoriesNavGraph(navController = navHostController)
-            }
+        inject()
+        setJetstoriesContent {
+            JetstoriesNavGraph(
+                navController = navHostController,
+                startDestination = LOGIN_SCREEN
+            )
         }
     }
 
