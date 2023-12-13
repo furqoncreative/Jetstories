04be9e7b0fd0925fd94b899d0fb5df6986260b49
Deden Muhamad Furqon <furqoncreative24@gmail.com>
2023-12-13 19:32:19 +0700

    Add `showErrorNameMustBeNotEmpty_whenEnteredBlankName` test case

--- app/src/androidTest/java/id/furqoncreative/jetstories/ui/screens/register/RegisterScreenKtTest.kt
+++ app/src/androidTest/java/id/furqoncreative/jetstories/ui/screens/register/RegisterScreenKtTest.kt
@@ -2,6 +2,7 @@ package id.furqoncreative.jetstories.ui.screens.register
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performImeAction
 import androidx.compose.ui.test.performTextReplacement
 import dagger.hilt.android.testing.HiltAndroidTest
 import id.furqoncreative.jetstories.JetstoriesScreenTest
@@ -26,7 +27,7 @@ class RegisterScreenKtTest : JetstoriesScreenTest() {
     }
 
     @Test
-    fun showEmailErrorSupportingText_whenEnteredInvalidEmail() = runTest {
+    fun showErrorEmailMustBeValid_whenEnteredInvalidEmail() = runTest {
         activity.apply {
             onNodeWithText(getString(R.string.email_label)).performTextReplacement("jet@gmail")
 
@@ -36,13 +37,34 @@ class RegisterScreenKtTest : JetstoriesScreenTest() {
     }
 
     @Test
-    fun showPasswordErrorPasswordDoesntMatch_whenEnteredDifferentPassword() = runTest {
+    fun showErrorPasswordMustBeValid_whenEnteredInvalidPassword() = runTest {
+        activity.apply {
+            onNodeWithText(getString(R.string.password_label)).performTextReplacement("1111")
+
+            waitForIdle()
+            onNodeWithText(getString(R.string.password_invalid_message)).assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun showErrorPasswordDoesntMatch_whenEnteredDifferentPassword() = runTest {
         activity.apply {
             onNodeWithText(getString(R.string.password_label)).performTextReplacement("1111")
             onNodeWithText(getString(R.string.confirmation_password_label)).performTextReplacement("2222")
 
             waitForIdle()
             onNodeWithText(getString(R.string.confirm_password_invalid_message)).assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun showErrorNameMustBeNotEmpty_whenEnteredBlankName() = runTest {
+        activity.apply {
+            onNodeWithText(getString(R.string.name_label)).performTextReplacement("")
+            onNodeWithText(getString(R.string.name_label)).performImeAction()
+
+            waitForIdle()
+            onNodeWithText(getString(R.string.name_invalid_message)).assertIsDisplayed()
         }
     }
 
